68785f59773d20fb6921265e9ebf68edab20f43a
Hongzf <1628415507@qq.com>
2022-10-12 11:25:45 +0800

    feat：交互控制、旋转模型

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: Hongzf
+ * @Date: 2022-10-10 15:15:41
+ * @LastEditors: Hongzf
+ * @LastEditTime: 2022-10-12 09:28:38
+ * @Description: 
+ */
 import { createApp } from 'vue'
 import './style.css'
+import Element3 from 'element3'
+import 'element3/lib/theme-chalk/index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(Element3).mount('#app')
